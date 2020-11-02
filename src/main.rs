7c6e6a4b70f40dbe7589d18268f25600487c6f80
MatejKastak <matej.kastak@gmail.com>
2020-11-02 16:09:40 +0100

    main: Play sound on notification pop-up

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,21 @@
-use notify_rust::{Hint, Notification};
+use notify_rust::Notification;
 use rand::seq::SliceRandom;
 use rand::thread_rng;
 use rand::Rng;
+use rodio::Source;
+use std::io::BufReader;
 use std::{thread, time};
 
-fn show_habit(habit: &str) -> u32 {
+fn show_habit(handle: &rodio::OutputStreamHandle, habit: &str) -> u32 {
     let mut sleep_time = 0;
+
+    play_notification_sound(handle);
+
     Notification::new()
         .summary(habit)
         .body("habi")
         .icon("firefox")
         .action("default", "default")
-        .hint(Hint::Resident(true))
         .show()
         .unwrap()
         .wait_for_action(|action| match action {
@@ -21,7 +25,15 @@ fn show_habit(habit: &str) -> u32 {
             "__closed" => sleep_time = 0,
             _ => sleep_time = 0,
         });
+
     return sleep_time;
+}
+
+fn play_notification_sound(handle: &rodio::OutputStreamHandle) {
+    let file = std::fs::File::open("/usr/share/sounds/freedesktop/stereo/message.oga").unwrap();
+
+    let source = rodio::Decoder::new(BufReader::new(file)).unwrap();
+    handle.play_raw(source.convert_samples()).unwrap();
 }
 
 fn main() {
@@ -54,6 +66,8 @@ fn main() {
         "Walk",
     ];
 
+    let (_stream, handle) = rodio::OutputStream::try_default().unwrap();
+
     let mut rng = thread_rng();
 
     loop {
@@ -63,7 +77,7 @@ fn main() {
         } else {
             short.choose(&mut rng).unwrap()
         };
-        let sleep_time = show_habit(habit);
+        let sleep_time = show_habit(&handle, habit);
         thread::sleep(time::Duration::from_secs((sleep_time * 60).into()))
     }
 }
